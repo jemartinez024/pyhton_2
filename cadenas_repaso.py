7934b89983bc3f3aaf1125599a34d2f47f5de31d
jemartinez024 <jemartinezflo@gmail.com>
2025-05-12 15:04:32 -0600

    Descripción de los cambios

---
+++ cadenas_repaso.py
@@ -0,0 +1,22 @@
+# Ejemplo 1
+
+word = 'by'
+print(len(word))
+
+
+# Ejemplo 2
+
+empty = ''
+print(len(empty))
+
+
+# Ejemplo 3
+
+i_am = 'I\'m'
+print(len(i_am))
+
+
+multiline = '''Línea #1
+Línea #2'''
+
+print(len(multiline))
---
+++ import_math.py
@@ -0,0 +1,10 @@
+import math
+
+result = math.e != math.pow(2, 4)
+print(int(result))
+
+from random import randint
+    
+for i in range(2):
+   print(randint(1, 2), end='')
+    
