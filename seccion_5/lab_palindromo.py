6ed611d7b4a607035d6096053bd2719299170341
jemartinez024 <jemartinezflo@gmail.com>
2025-05-16 15:21:09 -0600

    Descripción de los cambios

--- seccion_5/lab_palindromo.py
+++ seccion_5/lab_palindromo.py
@@ -1,13 +1,13 @@
 # Ingresa el texto a encriptar.
 text = input("Ingresa una palabra para comprobar si es un palíndromo: ")
-text = text.replace(' ','')  ## Elimina los espacios en blanco que pueda tener el IBAN.
-text = text.lower()
+text = text.replace(' ','')  ## Elimina los espacios en blanco que pueda tener
+text = text.lower() # Convierte a minúsculas.
 
-str = text
-inverted_string = str[::-1]
+input_str = text
+inverted_string = input_str[::-1]
 print(inverted_string)  # Salida:
 
-if str == inverted_string:
+if input_str == inverted_string:
     print('Es un palíndromo')
 else:
     print('No es un palíndromo')
