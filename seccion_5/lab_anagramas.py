f650821a6a5fa0ddaad187555b68614e123c1390
jemartinez024 <jemartinezflo@gmail.com>
2025-05-19 13:55:40 -0600

    Descripción de los cambios

---
+++ seccion_5/lab_anagramas.py
@@ -0,0 +1,22 @@
+print("*** Bienvendo al comprobador de Anagramas ***")
+
+# Ingresa el texto.
+text1 = input("Ingresa la pirmera palabra: ")
+text2 = input("Ingresa la segunda palabra: ")
+
+text1,text2 = text1,text2.replace(' ','')  ## Elimina los espacios en blanco que pueda tener
+text1,text2 = text1,text2.upper() # Convierte a minúsculas.
+
+char_list1 = list(text1) # Convierte cadenas en listas
+char_list2 = list(text2)
+
+orden1 = sorted(char_list1) # Ordena las listas
+orden2 = sorted(char_list2)
+
+string_add = "".join(orden1 + orden2) # Une los elementos de cada lista en cadenas separadas
+
+
+if orden1 == orden2:
+    print('Son anagramas')
+else:
+    print('No Son anagramas')
