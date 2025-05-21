ee179e9f4e18bc3604224b3d46ff1c0ba683782a
jemartinez024 <jemartinezflo@gmail.com>
2025-05-21 15:12:54 -0600

    Descripción de los cambios

---
+++ seccion_8/cuestionario.py
@@ -0,0 +1,17 @@
+"""
+2.8.4 CUESTIONARIO DE SECCIÓN
+Pregunta 1: ¿Cuál de las excepciones se utilizará para proteger al código de ser interrumpido por el uso del teclado?
+
+Ocultar
+KeyboardInterrupt
+Pregunta 2: ¿Cuál es el nombre de la más general de todas las excepciones de Python?
+
+Ocultar
+BaseException
+Pregunta 3: ¿Cuál de las excepciones será generada a través de la siguiente evaluación fallida?
+
+huge_value = 1E250 ** 2
+ 
+Ocultar
+OverflowError
+"""
---
+++ seccion_8/resumen.py
@@ -0,0 +1,18 @@
+"""
+2.8.3 RESUMEN DE SECCIÓN
+1. Algunas excepciones integradas abstractas de Python son:
+
+ArithmeticError,
+BaseException,
+LookupError.
+
+2. Algunas excepciones integradas concretas de Python son:
+
+AssertionError,
+ImportError,
+IndexError,
+KeyboardInterrupt,
+KeyError,
+MemoryError,
+OverflowError.
+"""
