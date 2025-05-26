4cf34f922968f39ec98b0a6f759ce960f90e819a
jemartinez024 <jemartinezflo@gmail.com>
2025-05-26 15:10:35 -0600

    Descripción de los cambios

---
+++ modulo_3_orientada_objetos/seccion1_fundamentos/cuestionario.py
@@ -0,0 +1,16 @@
+"""
+3.1.10 CUESTIONARIO DE SECCIÓN
+Pregunta 1: Si asumimos que pitones, víboras y cobras son subclases de la misma superclase, ¿cómo la llamarías?
+
+Ocultar
+Serpiente, reptil, vertebrado, animal: todas estas respuestas son aceptables.
+
+Pregunta 2: Intenta nombrar algunas subclases de las clase Pitón.
+
+Ocultar
+Pitón india, Pitón de Roca Africana, Pitón Bola, Pitón Birmana: la lista es larga.
+
+Pregunta 3: ¿Puedes usar la palabra "class" para darle nombre a alguna de tus clases?
+
+Ocultar
+¡No, no puedes, class es una palabra clave reservada!"""
