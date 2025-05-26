7f1df84876628d70ef6984e830b35aeb71133a41
jemartinez024 <jemartinezflo@gmail.com>
2025-05-26 14:16:06 -0600

    Descripción de los cambios

---
+++ seccion_8/prueba_modulo2.py
@@ -0,0 +1,269 @@
+"""Prueba del Módulo 2"""
+
+"""try:
+    print("5"/0)
+except ArithmeticError:
+    print("arit")
+except ZeroDivisionError:
+    print("cero")
+except:
+    print("algo")
+
+"""
+
+"""x = '\''
+print(len(x))
+
+"""
+
+"""#print(ord('c') - ord('a'))
+
+print(chr(ord('z') - 2))
+
+print(3 * 'abc' + 'xyz')
+
+print('Mike' > "Mikey")
+
+print(float("1, 3"))"""
+
+"""Prueba Módulo 2
+La siguiente prueba se basa en lo que acabas de aprender. Hay quince preguntas en total y debes obtener al menos un 70% para aprobar.
+
+¡Buena Suerte!
+
+
+Pregunta 1
+El entrar al bloque try: implica que:
+
+
+close
+todas las instrucciones de este bloque se ejecuten
+
+ninguna de las instrucciones de este bloque se ejecuten
+
+algunas de las instrucciones de este bloque no se ejecuten
+
+el bloqué será omitido
+Pregunta 2
+El bloque except: sin nombre:
+
+
+debe ser el primero
+
+se puede colocar en cualquier lugar
+
+no se puede utilizar si se ha utilizado algún bloque con nombre
+
+done
+debe ser el último
+Pregunta 3
+La excepción base en Python se llama:
+
+
+Exception
+
+done
+BaseException
+
+PythonException
+
+TopException
+Pregunta 4
+La siguiente sentencia:
+
+
+assert var == 0
+ 
+
+close
+es erronea
+
+detendrá el programa cuando var != 0
+
+detendrá el programa cuando var == 0
+
+no tiene efecto
+Pregunta 5
+¿Cuál es el resultado esperado del siguiente código?
+
+
+try:
+    print("5"/0)
+except ArithmeticError:
+    print("arit")
+except ZeroDivisionError:
+    print("cero")
+except:
+    print("algo")
+ 
+
+cero
+
+arit
+
+done
+algo
+
+0
+Pregunta 6
+¿Cuáles de las siguientes son ejemplos de excepciones integradas concretas de Python? (Selecciona dos respuestas)
+
+
+done
+ImportError
+
+BaseException
+
+done
+IndexError
+
+ArithmeticError
+Pregunta 7
+ASCII es:
+
+
+el nombre de una variable predefinida de Python
+
+el nombre de un Módulo Estándar de Python
+
+done
+la abreviatura de American Standard Code for Information Interchange
+
+el nombre de un carácter
+Pregunta 8
+UTF‑8 es:
+
+
+un sinónimo para la palabra byte
+
+el nombre de una versión de Python
+
+la novena versión del estándar UTF
+
+done
+una forma de codificar puntos de código Unicode
+Pregunta 9
+UNICODE es un estándar:
+
+
+honrado por todo el universo
+
+done
+como ASCII, pero mucho más expansivo
+
+empleado por programadores en universidades
+
+para codificar números punto flotante
+Pregunta 10
+El siguiente código:
+
+
+x = '\''
+print(len(x))
+ 
+imprime:
+
+
+2
+
+done
+1
+
+3
+
+20
+Pregunta 11
+El siguiente código:
+
+
+print(ord('c') - ord('a'))
+ 
+imprime:
+
+
+1
+
+done
+2
+
+0
+
+3
+Pregunta 12
+El siguiente código:
+
+
+print(chr(ord('z') ‑ 2))
+ 
+imprime:
+
+
+z
+
+a
+
+done
+x
+
+y
+Pregunta 13
+El siguiente código:
+
+
+print(3 * 'abc' + 'xyz')
+ 
+imprime:
+
+
+abcabcxyzxyz
+
+done
+abcabcabcxyz
+
+xyzxyzxyzxyz
+
+abcxyzxyzxyz
+Pregunta 14
+El siguiente código:
+
+
+print('Mike' > "Mikey")
+ 
+imprime:
+
+
+1
+
+0
+
+True
+
+done
+False
+Pregunta 15
+El siguiente código:
+
+
+print(float("1, 3"))
+ 
+
+done
+genera una excepción ValueError
+
+imprime 13
+
+imprime 1.3
+
+imprime 1,3
+¡Has enviado tus respuestas!
+done
+Reinicio
+Revisar evaluación
+87%
+Has obtenido un 87%.
+
+
+Felicidades, has acreditado la prueba.
+
+"""
+
