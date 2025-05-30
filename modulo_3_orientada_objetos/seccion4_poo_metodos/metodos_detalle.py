d2d10d8a79bd2c0c0990284ee0b9055597aba269
jemartinez024 <jemartinezflo@gmail.com>
2025-05-30 14:51:39 -0600

    Descripción de los cambios

--- modulo_3_orientada_objetos/seccion4_poo_metodos/metodos_detalle.py
+++ modulo_3_orientada_objetos/seccion4_poo_metodos/metodos_detalle.py
@@ -20,4 +20,94 @@ class Classy:
 
 obj = Classy()
 obj.method()
+
+"""El código da como salida:
+
+método
+Output
+Toma en cuenta la forma en que hemos creado el objeto, hemos tratado el nombre de la clase como una función, y devuelve un objeto recién instanciado de la clase.
+
+Si deseas que el método acepte parámetros distintos a self, debes:
+
+Colocarlos después de self en la definición del método.
+Pasarlos como argumentos durante la invocación sin especificar self.
+Justo como aqui:
+
+
+class Classy:
+    def method(self, par):
+        print("método:", par)
+ 
+ 
+obj = Classy()
+obj.method(1)
+obj.method(2)
+obj.method(3)
+ 
+El código da como salida:
+
+método: 1
+método: 2
+método: 3
+Output
+
+El parámetro self es usado para obtener acceso a la instancia del objeto y las variables de clase.
+
+
+class Classy:
+    varia = 2
+    def method(self):
+        print(self.varia, self.var)
+ 
+ 
+obj = Classy()
+obj.var = 3
+obj.method()
+ 
+El código da como salida:
+
+2 3
+Output
+
+El parámetro self también se usa para invocar otros métodos desde dentro de la clase.
+
+Justo como aquí:
+
+
+class Classy:
+    def other(self):
+        print("otro")
+ 
+    def method(self):
+        print("método")
+        self.other()
+ 
+ 
+obj = Classy()
+obj.method()
+ 
+El código da como salida:
+método
+otro
+Output
+
+Si se nombra un método de esta manera: __init__, no será un método regular, será un constructor.
+
+Si una clase tiene un constructor, este se invoca automática e implícitamente cuando se instancia el objeto de la clase.
+
+El constructor:
+
+Esta obligado a tener el parámetro self (se configura automáticamente).
+Pudiera (pero no necesariamente) tener mas parámetros que solo self; si esto sucede, la forma en que se usa el nombre de la clase para crear el objeto debe tener la definición __init__.
+Se puede utilizar para configurar el objeto, es decir, inicializa adecuadamente su estado interno, crea variables de instancia, crea instancias de cualquier otro objeto si es necesario, etc.
+Observa el código en el editor. El ejemplo muestra un constructor muy simple pero funcional."""
+
+class Classy:
+    def __init__(self, value):
+        self.var = value
+
+
+obj_1 = Classy("objeto")
+
+print(obj_1.var)
     
