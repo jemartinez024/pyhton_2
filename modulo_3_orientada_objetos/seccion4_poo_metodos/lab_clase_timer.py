403d3c61d62e88c52b7aa34017e4ba7aef015b6a
jemartinez024 <jemartinezflo@gmail.com>
2025-06-02 14:59:55 -0600

    Descripción de los cambios

--- modulo_3_orientada_objetos/seccion4_poo_metodos/lab_clase_timer.py
+++ modulo_3_orientada_objetos/seccion4_poo_metodos/lab_clase_timer.py
@@ -23,3 +23,55 @@ Output
 23:59:59
 00:00:00
 23:59:59"""
+
+
+class Timer:
+    def __init__( self, hours=0, minutes=0, seconds=0 ):
+        self.__hours = hours
+        self.__minutes = minutes
+        self.__seconds = seconds
+        #
+        # Escribir código aquí
+        #
+
+    def __str__(self):
+         return f"{self.__hours:02d}:{self.__minutes:02d}:{self.__seconds:02d}"
+        #
+        # Escribir código aquí
+        #
+
+    def next_second(self): 
+        self.__seconds += 1
+        if self.__seconds >= 60:
+            self.__seconds = 0
+            self.__minutes += 1
+            if self.__minutes >= 60:
+                self.__minutes = 0
+                self.__hours += 1
+                if self.__hours >= 24:
+                    self.__hours = 0
+        #
+        # Escribir código aquí
+        #
+
+    def prev_second(self):
+        self.__seconds -= 1
+        if self.__seconds < 0:
+            self.__seconds = 59
+            self.__minutes -= 1
+            if self.__minutes < 0:
+                self.__minutes = 59
+                self.__hours -= 1
+                if self.__hours < 0:
+                    self.__hours = 23
+        #
+        # Escribir código aquí
+        #
+
+
+timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
