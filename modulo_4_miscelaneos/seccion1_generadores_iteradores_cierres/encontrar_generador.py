fcfc508c6fee4e6479810f95c35afb96144f306d
jemartinez024 <jemartinezflo@gmail.com>
2025-06-10 13:49:08 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion1_generadores_iteradores_cierres/encontrar_generador.py
@@ -0,0 +1,5 @@
+"""4.1.1 Generadores: donde encontrarlos
+
+Generador - ¿Con qué asocias esta palabra? Quizás se refiere a algún dispositivo electrónico. O tal vez se refiere a una máquina pesada diseñada para producir energía eléctrica u otra cosa.
+
+Un generador de Python es un fragmento de código especializado capaz de producir una serie de valores y controlar el proceso de iteración. Esta es la razón por la cual los generadores a menudo se llaman iteradores, y aunque hay quienes pueden encontrar una diferencia entre estos dos, aquí los trataremos como uno mismo."""
