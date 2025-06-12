203f227c5f2ff429bbdcb8bb08f59c752e1fb145
jemartinez024 <jemartinezflo@gmail.com>
2025-06-12 13:56:38 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion1_generadores_iteradores_cierres/lambdas_map.py
+++ modulo_4_miscelaneos/seccion1_generadores_iteradores_cierres/lambdas_map.py
@@ -20,3 +20,12 @@ Puedes usar el iterador resultante en un bucle o convertirlo en una lista usando
 ¿Puedes ver un papel para una lambda aquí?
 
 Observa el código en el editor: hemos usado dos lambdas en él."""
+
+list_1 = [x for x in range(5)]
+list_2 = list(map(lambda x: 2 ** x, list_1))
+print(list_2)
+
+for x in map(lambda x: x * x, list_2):
+    print(x, end=' ')
+print()
+    
