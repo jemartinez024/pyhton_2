dd7f6a21bb13b4bfcaa1c81373d679e9dc874fed
jemartinez024 <jemartinezflo@gmail.com>
2025-06-12 13:06:45 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion1_generadores_iteradores_cierres/construir_generador.py
+++ modulo_4_miscelaneos/seccion1_generadores_iteradores_cierres/construir_generador.py
@@ -24,7 +24,7 @@ for v in fun(5):
 4"""
 
 
-def powers_of_2(n):
+"""def powers_of_2(n):
     power = 1
     for i in range(n):
         yield power
@@ -32,5 +32,93 @@ def powers_of_2(n):
 
 
 for v in powers_of_2(8):
-    print(v)
+    print(v)"""
 
+"""Listas por comprensión
+
+Los generadores también se pueden usar con listas por comprensión, justo como aquí:
+
+def powers_of_2(n):
+    power = 1
+    for i in range(n):
+        yield power
+        power *= 2
+ 
+ 
+t = [x for x in powers_of_2(5)]
+print(t)
+
+Output:
+[1, 2, 4, 8, 16]
+
+"""
+"""Ejecuta el ejemplo y verifica la salida.
+
+
+La función list()
+
+La función list() puede transformar una serie de invocaciones de generador subsequentes en una lista real:
+
+def powers_of_2(n):
+    power = 1
+    for i in range(n):
+        yield power
+        power *= 2
+ 
+ 
+t = list(powers_of_2(3))
+print(t)
+
+Nuevamente, intenta predecir el resultado y ejecuta el código para verificar tus predicciones.
+ 
+output:
+[1, 2, 4] 
+
+"""
+
+"""El operador in
+
+Además, el contexto creado por el operador in también te permite usar un generador.
+
+El ejemplo muestra cómo hacerlo:
+
+def powers_of_2(n):
+    power = 1
+    for i in range(n):
+        yield power
+        power *= 2
+ 
+ 
+for i in range(20):
+    if i in powers_of_2(4):
+        print(i)
+ 
+
+¿Cuál es la salida del código? Ejecuta el programa y verifica.
+
+"""
+"""El generador de números Fibonacci
+
+Ahora veamos un generador de números de la serie Fibonacci, asegurandonos que se vea mucho mejor que la versión orientada a objetos basada en la implementación directa del protocolo iterador.
+
+Aquí está:
+
+
+def fibonacci(n):
+    p = pp = 1
+    for i in range(n):
+        if i in [0, 1]:
+            yield 1
+        else:
+            n = p + pp
+            pp, p = p, n
+            yield n
+ 
+fibs = list(fibonacci(10))
+print(fibs)
+ 
+
+Adivina la salida (una lista) producida por el generador y ejecuta el código para verificar si tenías razón."""
+
+# Output:
+# [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
