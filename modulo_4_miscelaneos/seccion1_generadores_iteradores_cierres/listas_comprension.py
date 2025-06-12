8794edc0fb26b79dd905c3d937861774a23b0e59
jemartinez024 <jemartinezflo@gmail.com>
2025-06-12 13:08:02 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion1_generadores_iteradores_cierres/listas_comprension.py
@@ -0,0 +1,4 @@
+"""4.1.4 Más acerca de listas por comprensión
+Debes poder recordar las reglas que rigen la creación y el uso de un fenómeno de Python llamado listas por comprensión: una forma simple de crear listas y sus contenidos.
+
+En caso de que lo necesites, te proporcionamos un recordatorio en el editor."""
