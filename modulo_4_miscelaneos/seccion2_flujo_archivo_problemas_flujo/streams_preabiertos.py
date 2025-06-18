3ff19ba75414bb2e18da6555717458e3f4e534b2
jemartinez024 <jemartinezflo@gmail.com>
2025-06-18 15:41:26 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion2_flujo_archivo_problemas_flujo/streams_preabiertos.py
+++ modulo_4_miscelaneos/seccion2_flujo_archivo_problemas_flujo/streams_preabiertos.py
@@ -29,3 +29,21 @@ El stream stderr normalmente está asociado con la pantalla, preabierta para esc
 No hemos presentado ningún método para enviar datos a este stream (lo haremos pronto, lo prometemos).
 La separación de stdout (resultados útiles producidos por el programa) de stderr (mensajes de error, indudablemente útiles pero no proporcionan resultados) ofrece la posibilidad de redirigir estos dos tipos de información a los diferentes objetivos. Una discusión más extensa sobre este tema está más allá del alcance de nuestro curso. El manual del sistema operativo proporcionará más información sobre estos temas.
 """
+
+"""
+4.2.9 Cerrando los flujos (streams)
+La última operación realizada en un stream (esto no incluye a los streams stdin, stdout, y stderr pues no lo requieren) debe ser cerrarlo.
+
+Esa acción se realiza mediante un método invocado desde dentro del objeto del stream: stream.close().
+
+El nombre de la función es fácil de entender close(), es decir cerrar.
+La función no espera argumentos; el stream no necesita estar abierto.
+La función no devuelve nada pero genera una excepción IOError en caso de un error.
+La mayoría de los desarrolladores creen que la función close() siempre tiene éxito y, por lo tanto, no hay necesidad de verificar si ha realizado su tarea correctamente.
+
+Esta creencia está solo parcialmente justificada. Si el stream se abrió para escribir y luego se realizó una serie de operaciones de escritura, puede ocurrir que los datos enviados al stream aún no se hayan transferido al dispositivo físico (debido a los mecanismos de cache o buffer).
+
+Dado que el cierre del stream obliga a los bufers a descargarse, es posible que dichas descargas fallen y, por lo tanto, close() falle también.
+Ya hemos mencionado fallas causadas por funciones que operan con los streams, pero no mencionamos nada sobre cómo podemos identificar exactamente la causa de la falla.
+
+La posibilidad de hacer un diagnóstico existe y es proporcionada por uno de los componentes de excepción de los streams. Hablaremos acerca de ellos a continuación."""
