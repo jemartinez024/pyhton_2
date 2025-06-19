927f583ccadd05eafa2fcfd1f56b6eac4cde805d
jemartinez024 <jemartinezflo@gmail.com>
2025-06-19 14:06:33 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/procesando_archivos_texto.py
+++ modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/procesando_archivos_texto.py
@@ -46,3 +46,57 @@ Simple es mejor que complejo.
 Complejo es mejor que complicado.
 
 Ahora observa el código en el editor y analicémoslo."""
+
+
+from os import strerror
+
+try:
+    counter = 0
+    stream = open('text.txt', "rt")
+    char = stream.read(1)
+    while char != '':
+        print(char, end='')
+        counter += 1
+        char = stream.read(1)
+    stream.close()
+    print("\n\nCaracteres en el archivo:", counter)
+except IOError as e:
+    print("Se produjo un error de E/S: ", strerror(e.errno))
+
+"""La rutina es bastante simple:
+
+Se usa el mecanismo try-except y se abre el archivo con el nombre (text.txt en este caso).
+Intenta leer el primer carácter del archivo (char = stream.read(1)).
+Si tienes éxito (esto se demuestra por el resultado positivo de la condición while), se muestra el carácter (nota el argumento end=, ¡es importante! ¡No querrás saltar a una nueva línea después de cada carácter!).
+Tambien, se actualiza el contador (counter).
+Intenta leer el siguiente carácter y el proceso se repite.
+Si estás absolutamente seguro de que la longitud del archivo es segura y puedes leer todo el archivo en la memoria de una vez, puedes hacerlo: la función read(), invocada sin ningún argumento o con un argumento que se evalúa a None, hará el trabajo por ti.
+
+Recuerda: el leer un archivo muy grande (en terabytes) usando este método puede dañar tu sistema operativo.
+
+No esperes milagros: la memoria de la computadora no se puede extender.
+
+Observa el código en el editor. ¿Qué piensas de el?"""
+
+
+from os import strerror
+
+try:
+    counter = 0
+    stream = open('text.txt', "rt")
+    content = stream.read()
+    for char in content:
+        print(char, end='')
+        counter += 1
+    stream.close()
+    print("\n\nCaracteres en el archivo:", counter)
+except IOError as e:
+    print("Se produjo un error de E/S: ", strerr(e.errno))
+  
+  
+"""Vamos a analizarlo:
+
+Abre el archivo, como anteriormente se hizo.
+Lee el contenido mediante una invocación de la función read().
+Después, se procesa el texto, iterando con un bucle for su contenido, y se actualiza el valor del contador en cada vuelta del bucle.
+El resultado será exactamente el mismo que en el ejemplo anterior."""
