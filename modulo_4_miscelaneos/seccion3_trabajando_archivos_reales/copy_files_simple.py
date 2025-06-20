9ddab7a46c900dbfd52b31cefb7cfc4538b83df2
jemartinez024 <jemartinezflo@gmail.com>
2025-06-20 14:20:55 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/copy_files_simple.py
+++ modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/copy_files_simple.py
@@ -5,3 +5,37 @@ Ahora vas a juntar todo este nuevo conocimiento, agregarle algunos elementos nue
 Por supuesto, el propósito no es crear un reemplazo para los comandos como copy de (MS Windows) o cp de (Unix/Linux) pero para ver una forma posible de crear una herramienta de trabajo, incluso si nadie quiere usarla.
 
 Observa el código en el editor."""
+
+from os import strerror
+
+srcname = input("Ingresa el nombre del archivo fuente: ")
+try:
+    src = open(srcname, 'rb')
+except IOError as e:
+    print("No se puede abrir archivo fuente: ", strerror(e.errno))
+    exit(e.errno)	
+
+dstname = input("Ingresa el nombre del archivo destino: ")
+try:
+    dst = open(dstname, 'wb')
+except Exception as e:
+    print("No se puede crear el archivo de destino: ", strerror(e.errno))
+    src.close()
+    exit(e.errno)	
+
+buffer = bytearray(65536)
+total  = 0
+try:
+    readin = src.readinto(buffer)
+    while readin > 0:
+        written = dst.write(buffer[:readin])
+        total += written
+        readin = src.readinto(buffer)
+except IOError as e:
+    print("No se puede crear el archivo de destino: ", strerror(e.errno))
+    exit(e.errno)	
+    
+print(total,'byte(s) escritos con éxito')
+src.close()
+dst.close()
+    
