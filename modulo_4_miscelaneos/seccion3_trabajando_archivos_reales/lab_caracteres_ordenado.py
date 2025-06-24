d8dfbfa4fe8c25562344fe5e02cda55c9e9db25b
jemartinez024 <jemartinezflo@gmail.com>
2025-06-24 15:52:24 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/lab_caracteres_ordenado.py
@@ -0,0 +1,20 @@
+"""4.3.9   LAB   Histograma de frecuencia de caracteres ordenado
+El código anterior necesita ser mejorado. Está bien, pero tiene que ser mejor.
+
+Tu tarea es hacer algunas enmiendas, que generen los siguientes resultados:
+
+El histograma de salida se ordenará en función de la frecuencia de los caracteres (el contador más grande debe presentarse primero).
+El histograma debe enviarse a un archivo con el mismo nombre que el de entrada, pero con la extensión '.hist' (debe concatenarse con el nombre original).
+Suponiendo que el archivo de prueba contiene solo una línea con:
+
+cBabAa
+samplefile.txt
+New Component Title
+El resultado esperado debería verse de la siguiente manera:
+
+Output
+a -> 3
+b -> 2
+c -> 1
+
+Consejo: Emplea una lambda para cambiar el ordenamiento."""
