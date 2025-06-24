d5b415c5d6f3550cb4b5ffeff3b50007e9724718
jemartinez024 <jemartinezflo@gmail.com>
2025-06-24 15:15:39 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/lab_histograma.py
+++ modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/lab_histograma.py
@@ -6,3 +6,16 @@ try:
 except FileNotFoundError:
     print("El archivo no existe.")
 
+letters_count = {}
+for char in content:
+    if char.isalpha():
+        char = char.lower()
+        if char in letters_count:
+            letters_count[char] += 1
+        else:
+            letters_count[char] = 1
+
+letters_order = sorted(letters_count.items(), key=lambda x: x[1], reverse=True)
+
+for letter in letters_order:
+    print(f"{letter} -> {letters_count[letter]}")  
