8ef6a9648d28e547d557b239eecaa4619ff869d2
jemartinez024 <jemartinezflo@gmail.com>
2025-06-25 13:59:12 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/lab_caracteres_ordenado.py
+++ modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/lab_caracteres_ordenado.py
@@ -18,3 +18,25 @@ b -> 2
 c -> 1
 
 Consejo: Emplea una lambda para cambiar el ordenamiento."""
+
+samplefile = input("Introduce el nombre del archivo: ")
+
+try:
+    with open(samplefile, 'r') as file:
+        content = file.read()
+except FileNotFoundError:
+    print("El archivo no existe.")
+
+letters_count = {}
+for char in content:
+    if char.isalpha():
+        char = char.lower()
+        if char in letters_count:
+            letters_count[char] += 1
+        else:
+            letters_count[char] = 1
+
+letters_order = sorted(letters_count.keys())
+
+for letter in letters_order:
+    print(f"{letter} -> {letters_count[letter]}")  
