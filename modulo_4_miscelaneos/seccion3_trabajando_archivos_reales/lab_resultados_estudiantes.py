f3aaa5d01cc2439392bf465a082bf0a404bb0583
jemartinez024 <jemartinezflo@gmail.com>
2025-06-25 15:20:56 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion3_trabajando_archivos_reales/lab_resultados_estudiantes.py
@@ -0,0 +1,19 @@
+"""4.3.10   LAB   Evaluando los resultados de los estudiantes
+El profesor Jekyll dirige clases con estudiantes y regularmente toma notas en un archivo de texto. Cada línea del archivo contiene 3 elementos: el nombre del alumno, el apellido del alumno y el número de puntos que el alumno recibió durante ciertas clases.
+
+Los elementos están separados con espacios en blanco. Cada estudiante puede aparecer más de una vez dentro del archivo del profesor Jekyll.
+
+El archivo puede tener el siguiente aspecto:
+
+John     Smith    5
+Anna     Boleyn   4.5
+John     Smith    2
+Anna     Boleyn   11
+Andrew   Cox      1.5
+samplefile.txt
+New Component Title
+Tu tarea es escribir un programa que:
+
+Pida al usuario el nombre del archivo del profesor Jekyll.
+Lea el contenido del archivo y cuenta la suma de los puntos recibidos por cada estudiante.
+Imprima un informe simple (pero ordenado), como este:"""
