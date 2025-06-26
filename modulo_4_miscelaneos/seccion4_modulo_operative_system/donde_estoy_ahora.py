98c677ea2b7072f62d6b29206b2ec68d33d265b0
jemartinez024 <jemartinezflo@gmail.com>
2025-06-26 14:42:30 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion4_modulo_operative_system/donde_estoy_ahora.py
+++ modulo_4_miscelaneos/seccion4_modulo_operative_system/donde_estoy_ahora.py
@@ -16,4 +16,9 @@ os.chdir("my_first_directory")
 print(os.getcwd())
 os.chdir("my_second_directory")
 print(os.getcwd())
-    
+
+"""En el ejemplo, creamos el directorio my_first_directory y el directorio my_second_directory dentro de él. En el siguiente paso, cambiamos el directorio de trabajo actual al directorio my_first_directory y luego mostramos el directorio de trabajo actual (primera línea del resultado).
+
+A continuación, vamos al directorio my_second_directory y nuevamente mostramos el directorio de trabajo actual (segunda línea del resultado). Como puedes ver, la función getcwd devuelve la ruta absoluta a los directorios.
+
+NOTA: En sistemas tipo Unix, el equivalente de la función getcwd es el comando pwd, que imprime el nombre del directorio de trabajo actual."""
