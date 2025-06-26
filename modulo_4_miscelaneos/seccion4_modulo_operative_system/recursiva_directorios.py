7a6a7566e75f676c18b1756ea613bde0599eb3e8
jemartinez024 <jemartinezflo@gmail.com>
2025-06-26 14:36:42 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion4_modulo_operative_system/recursiva_directorios.py
@@ -0,0 +1,4 @@
+"""4.4.4 Creación recursiva de directorios
+La función mkdir es muy útil, pero ¿qué sucede si necesitas crear otro directorio dentro del directorio que acabas de crear? Por supuesto, puedes ir al directorio creado y crear otro directorio dentro de él, pero afortunadamente el módulo os proporciona una función llamada makedirs, que facilita esta tarea.
+
+La función makedirs permite la creación recursiva de directorios, lo que significa que se crearán todos los directorios de la ruta. Veamos el código en el editor y veamos cómo es en la práctica."""
