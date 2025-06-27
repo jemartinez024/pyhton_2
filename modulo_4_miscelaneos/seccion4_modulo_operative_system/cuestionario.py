89038ab900f25ad98b85a9b22ea970e09a0435fb
jemartinez024 <jemartinezflo@gmail.com>
2025-06-27 14:38:35 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion4_modulo_operative_system/cuestionario.py
@@ -0,0 +1,18 @@
+"""
+4.4.10 CUESTIONARIO DE SECCIÓN
+Pregunta 1: ¿Cuál es el resultado del siguiente fragmento si se ejecuta en Unix?
+
+import os
+print(os.name)
+ 
+Ocultar
+posix
+Pregunta 2: ¿Cuál es el resultado del siguiente fragmento de código?
+
+import os
+ 
+os.mkdir("hello")
+print(os.listdir())
+ 
+Ocultar
+['hello']"""
