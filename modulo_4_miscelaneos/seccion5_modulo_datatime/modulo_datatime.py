bbd19418366254a03e9726224cc627d094e48967
jemartinez024 <jemartinezflo@gmail.com>
2025-06-30 13:26:48 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion5_modulo_datatime/modulo_datatime.py
@@ -0,0 +1,17 @@
+"""Bienvenido a la sección cinco, donde aprenderás sobre el módulo datetime y te familiarizarás con las clases de Python para trabajar con la fecha y la hora.
+
+4.5.1 Introducción al módulo datetime
+En esta sección, aprenderás sobre un módulo de Python llamado datetime.
+
+Como puedes adivinar, proporciona clases para trabajar con la fecha y hora. Si crees que no necesitas profundizar en este tema, hablemos de ejemplos del uso de la fecha y la hora en la programación.
+
+La fecha y la hora tienen innumerables usos y probablemente sea difícil encontrar una aplicación de producción que no los utilice. A continuación, se muestran algunos ejemplos:
+
+
+Registro de eventos - gracias al conocimiento de la fecha y la hora, podemos determinar cuándo ocurre exactamente un error crítico en nuestra aplicación. Al crear registros, puedes especificar el formato de fecha y hora.
+
+Seguimiento de cambios en la base de datos - a veces es necesario almacenar información sobre cuándo se creó o modificó un registro. El módulo datetime será perfecto para este caso.
+
+Validación de datos - pronto aprenderás a leer la fecha y hora actuales en Python. Conociendo la fecha y hora actuales, podrás validar varios tipos de datos, por ejemplo, si un cupón de descuento ingresado por un usuario en nuestra aplicación sigue siendo válido.
+
+Almacenamiento de información importante - ¿te imaginas las transferencias bancarias sin almacenar la información de cuándo se realizaron? La fecha y la hora de ciertas acciones deben conservarse y debemos ocuparnos de ello."""
