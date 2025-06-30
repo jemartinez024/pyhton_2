6f745414d3820fe15d5d67bcc8759d3b1c96c942
jemartinez024 <jemartinezflo@gmail.com>
2025-06-30 13:54:52 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion5_modulo_datatime/objeto_fecha_marca_tiempo.py
@@ -0,0 +1,20 @@
+"""4.5.3 Creación de un objeto de fecha a partir de una marca de tiempo
+La clase date nos da la capacidad de crear un objeto del tipo fecha a partir de una marca de tiempo.
+
+En Unix, la marca de tiempo expresa el número de segundos desde el 1 de Enero de 1970 a las 00:00:00 (UTC). Esta fecha se llama la época Unix, porque es cuando comenzó el conteo del tiempo en los sistemas Unix.
+
+La marca de tiempo es en realidad la diferencia entre una fecha en particular (incluida la hora) y el 1 de enero de 1970, 00:00:00 (UTC), expresada en segundos.
+
+Para crear un objeto de fecha a partir de una marca de tiempo, debemos pasar una marca de tiempo Unix al método fromtimestamp.
+
+Para este propósito, podemos usar el módulo time, que proporciona funciones relacionadas con el tiempo. Uno de ellos es una función llamada time(), que devuelve el número de segundos desde el 1 de enero de 1970 hasta el momento actual en forma de número flotante. Echa un vistazo al ejemplo en el editor."""
+
+from datetime import date
+import time
+
+timestamp = time.time()
+print("Marca de tiempo:", timestamp)
+
+d = date.fromtimestamp(timestamp)
+print("Fecha:", d)
+    
