dd2f2714d94c1bf261f1edb7b796fcf26cfe0859
jemartinez024 <jemartinezflo@gmail.com>
2025-06-30 13:45:47 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion5_modulo_datatime/obtener_fecha_hora.py
+++ modulo_4_miscelaneos/seccion5_modulo_datatime/obtener_fecha_hora.py
@@ -12,3 +12,26 @@ print("Año:", today.year)
 print("Mes:", today.month)
 print("Día:", today.day)
     
+"""El método today devuelve un objeto del tipo date que representa la fecha local actual. Toma en cuenta que el objeto date tiene tres atributos: año, mes y día.
+
+Ten cuidado, porque estos atributos son de solo lectura. Para crear un objeto date, debes pasar los parámetros año, mes y día de la siguiente manera:"""
+
+from datetime import date
+
+my_date = date(2012, 11, 4)
+print(my_date)
+    
+    
+"""Ejecuta el ejemplo para ver qué sucede.
+
+Al crear un objeto date, toma en cuenta las siguientes restricciones:
+
+
+Parametro:      Restricciones
+
+year            El parametro year debe ser mayor o igual a 1 (constante MINYEAR)
+                yb menor o igual a 9999 (contante MAXYEAR).
+month           El parametro month debe ser mayor o igual a 1 y menor o igual a               12
+day             El parametro day debe ser mayor o igual a 1 y menor o igual que             que el ultimo día del mes y año indicados
+
+Nota: más adelante en este curso, aprenderás a cambiar el formato de fecha predeterminado."""
