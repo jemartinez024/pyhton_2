43ea1436617c96b53c7debd4130cb75b4f147bc9
jemartinez024 <jemartinezflo@gmail.com>
2025-07-01 15:21:22 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion5_modulo_datatime/formato_fecha_hora.py
@@ -0,0 +1,6 @@
+"""4.5.15 Dando formato a la fecha y hora
+Todas las clases del módulo datetime presentadas hasta ahora tienen un método llamado strftime. Este es un método muy importante, porque nos permite devolver la fecha y la hora en el formato que especificamos.
+
+El método strftime toma solo un argumento en forma de cadena que especifica un formato que puede constar de directivas.
+
+Una directiva es una cadena que consta del carácter % (porcentaje) y una letra minúscula o mayúscula. Por ejemplo, la directiva %Y significa el año con el siglo como número decimal. Veámoslo en un ejemplo. Ejecuta el código en el editor."""
