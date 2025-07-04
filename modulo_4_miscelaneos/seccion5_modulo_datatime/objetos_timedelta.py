89595e357d54d00a163c7a78b7da1d6832354f52
jemartinez024 <jemartinezflo@gmail.com>
2025-07-04 12:52:27 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion5_modulo_datatime/objetos_timedelta.py
+++ modulo_4_miscelaneos/seccion5_modulo_datatime/objetos_timedelta.py
@@ -9,3 +9,51 @@ from datetime import timedelta
 
 delta = timedelta(weeks=2, days=2, hours=3)
 print(delta)
+
+"""Resultado:
+
+Output
+16 days, 3:00:00
+
+El resultado de 16 días se obtiene convirtiendo el argumento weeks en días (2 semanas = 14 días) y agregando el argumento days (2 días). Este es un comportamiento normal, porque el objeto timedelta solo almacena días, segundos y microsegundos internamente. De manera similar, el argumento hora se convierte en minutos. Echa un vistazo al siguiente ejemplo:
+
+
+from datetime import timedelta
+ 
+delta = timedelta(weeks=2, days=2, hours=3)
+print("Días:", delta.days)
+print("Segundos:", delta.seconds)
+print("Microsegundos:", delta.microseconds)
+ 
+
+Resultado:
+
+Output
+Días: 16
+Segundos: 10800
+Microsegundos: 0
+El resultado de 10800 se obtiene convirtiendo 3 horas en segundos. De esta forma el objeto timedelta almacena los argumentos pasados durante su creación. Las semanas se convierten en días, las horas y los minutos en segundos y los milisegundos en microsegundos.
+
+
+Ya sabes cómo el objeto timedelta almacena los argumentos pasados internamente. Veamos cómo se puede utilizar en la práctica.
+
+Observa algunas operaciones admitidas por las clases del módulo datetime. Ejecuta el código que te proporcionamos en el editor."""
+
+
+from datetime import timedelta
+from datetime import date
+from datetime import datetime
+
+delta = timedelta(weeks=2, days=2, hours=2)
+print(delta)
+
+delta2 = delta * 2
+print(delta2)
+
+d = date(2019, 10, 4) + delta2
+print(d)
+
+dt = datetime(2019, 10, 4, 14, 53) + delta2
+print(dt)
+    
+
