df79bab96cc0fc0390204fca43de31695cf902e6
jemartinez024 <jemartinezflo@gmail.com>
2025-07-04 13:05:53 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion5_modulo_datatime/lab_datetime_time.py
@@ -0,0 +1,17 @@
+"""4.5.20   LAB   Los módulos datetime y time
+Durante este curso, has aprendido sobre el método strftime, que requiere conocimiento de las directivas para crear un formato. Ahora es el momento de poner en práctica estas directivas.
+
+Por cierto, tendrás la oportunidad de practicar el trabajo con documentación, porque tendrás que encontrar directivas que aún no conoces.
+
+Aquí está tu tarea:
+Escribe un programa que cree un objeto datetime para el 4 de noviembre de 2020, 14:53:00. El objeto creado debe llamar al método strftime con el formato apropiado para mostrar el siguiente resultado:
+
+Output
+2020/11/04 14:53:00
+20/November/04 14:53:00 PM
+Wed, 2020 Nov 04
+Wednesday, 2020 November 04
+Día de la semana: 3
+Día del año: 309
+Número de semana en el año: 44
+Nota: Cada línea de resultado debe crearse llamando al método strftime con al menos una directiva en el argumento de formato."""
