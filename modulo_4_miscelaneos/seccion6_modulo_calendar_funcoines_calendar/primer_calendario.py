d0c91ea1d2daafe71d351f4aebb5006ef02c890f
jemartinez024 <jemartinezflo@gmail.com>
2025-07-07 13:32:49 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion6_modulo_calendar_funcoines_calendar/primer_calendario.py
@@ -0,0 +1,5 @@
+"""4.6.2 Tu primer calendario
+Comenzarás tu aventura con el módulo calendar con una función simple llamada calendar, que te permite mostrar el calendario para todo el año. Veamos cómo usarlo para mostrar el calendario de 2020. Ejecuta el código en el editor y ve qué sucede."""
+
+import calendar
+print(calendar.calendar(2020))
