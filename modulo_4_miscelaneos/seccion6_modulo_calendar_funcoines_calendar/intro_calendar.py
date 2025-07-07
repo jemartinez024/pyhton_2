3c359cbd6c79f76cf6d140743dcc3a093cbed826
jemartinez024 <jemartinezflo@gmail.com>
2025-07-07 13:26:31 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion6_modulo_calendar_funcoines_calendar/intro_calendar.py
@@ -0,0 +1,4 @@
+"""4.6.1 Introducción al módulo calendar
+Además de los módulos datetime y time, la biblioteca estándar de Python proporciona un módulo llamado calendar que, como su nombre indica, ofrece funciones relacionadas con el calendario.
+
+Uno de ellos es, por supuesto, mostrar el calendario. Es importante que los días de la semana se muestren de lunes a domingo, y cada día de la semana tiene su representación en forma de número entero:"""
