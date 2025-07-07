1fb2165a8cef3ca360b94daf042a65d78ce0c0f2
jemartinez024 <jemartinezflo@gmail.com>
2025-07-07 13:54:20 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion6_modulo_calendar_funcoines_calendar/crear_calendarios.py
@@ -0,0 +1,11 @@
+"""4.6.8 Clases para la creación de calendarios
+Las funciones que hemos mostrado hasta ahora no son todo lo que ofrece el módulo calendar. Además de ellos, podemos utilizar las siguientes clases:
+
+calendar.Calendar - proporciona métodos para preparar datos de calendario y dar formato.
+calendar.TextCalendar - se utiliza para crear calendarios de texto regulares.
+calendar.HTMLCalendar: se utiliza para crear calendarios HTML.
+calendar.LocalTextCalendar - es una subclase de la clase calendar.TextCalendar. El constructor de esta clase toma el parámetro locale, el cual se utiliza para devolver los nombres apropiados de los meses y días de la semana.
+calendar.LocalHTMLCalendar - es una subclase de la clase calendar.HTMLCalendar. El constructor de esta clase toma el parámetro locale, que se usa para devolver los nombres apropiados de los meses y días de la semana.
+Durante este curso, ya tuviste la oportunidad de crear calendarios de texto al discutir las funciones del módulo calendar.
+
+Es hora de probar algo nuevo. Echemos un vistazo más de cerca a los métodos de la clase calendar."""
