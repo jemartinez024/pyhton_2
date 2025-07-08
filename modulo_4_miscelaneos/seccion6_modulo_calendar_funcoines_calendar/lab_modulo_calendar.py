2bf4d72db46feb728b29321ea21d4e325aaf9f65
jemartinez024 <jemartinezflo@gmail.com>
2025-07-08 14:03:14 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion6_modulo_calendar_funcoines_calendar/lab_modulo_calendar.py
@@ -0,0 +1,26 @@
+"""Durante este curso, echamos un breve vistazo a la clase Calendar. Tu tarea ahora es ampliar su funcionalidad con un nuevo método llamado count_weekday_in_year, que toma un año y un día de la semana como parámetros, y luego devuelve el número de ocurrencias de un día de la semana específico en el año.
+
+Utiliza los siguientes consejos:
+
+Crea una clase llamada MyCalendar que se extiende de la clase Calendar.
+Crea el método count_weekday_in_year con los parámetros de year y weekday. El parámetro weekday debe tener un valor entre 0 y 6, donde 0 es el lunes y 6 es el domingo. El método debe devolver el número de días como un número entero.
+En tu implementación, usa el método monthdays2calendar de la clase Calendar.
+Los siguientes son resultados esperados de ejemplo:
+
+Argumentos de muestra
+
+Output
+year=2019, weekday=0
+New Component Title
+Salida esperada
+
+Output
+52
+
+Argumentos de muestra
+
+year=2000, weekday=6
+Salida esperada
+
+Output
+53"""
