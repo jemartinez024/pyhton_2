8c3888170b32df4e37be1c6918bf4437370ffece
jemartinez024 <jemartinezflo@gmail.com>
2025-07-08 14:50:10 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion6_modulo_calendar_funcoines_calendar/cuestionario.py
@@ -0,0 +1,22 @@
+"""
+4.6.15 CUESTIONARIO DE SECCIÓN
+New Component Title
+Pregunta 1: ¿Cuál es el resultado del siguiente fragmento de código?
+
+import calendar
+print(calendar.weekheader(1))
+ 
+Ocultar
+M T W T F S S
+Pregunta 2: ¿Cuál es el resultado del siguiente fragmento de código?
+
+import calendar
+ 
+c = calendar.Calendar()
+ 
+for weekday in c.iterweekdays():
+    print(weekday, end=" ")
+ 
+Ocultar
+0 1 2 3 4 5 6
+"""
