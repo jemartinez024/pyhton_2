ed658c6e426e1130c3898cc857e5864637a8a43c
jemartinez024 <jemartinezflo@gmail.com>
2025-07-08 14:59:55 -0600

    Descripción de los cambios

---
+++ modulo_4_miscelaneos/seccion6_modulo_calendar_funcoines_calendar/prueba_modulo4.py
@@ -0,0 +1,8 @@
+my_list = [1, 2, 3]
+foo = tuple(map(lambda x: x**x, my_list))# Insertar línea de código aquí.
+print(foo)
+
+my_tuple = (0, 1, 2, 3, 4, 5, 6)
+foo = tuple(filter(lambda x: x>1, my_tuple)) # Insertar línea de código aquí.
+print(foo)
+
