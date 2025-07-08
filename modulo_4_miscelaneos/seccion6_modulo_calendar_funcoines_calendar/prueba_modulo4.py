9e0a2e505d74f18eed96b722873b911ca8b55b38
jemartinez024 <jemartinezflo@gmail.com>
2025-07-08 15:01:35 -0600

    Descripción de los cambios

--- modulo_4_miscelaneos/seccion6_modulo_calendar_funcoines_calendar/prueba_modulo4.py
+++ modulo_4_miscelaneos/seccion6_modulo_calendar_funcoines_calendar/prueba_modulo4.py
@@ -6,3 +6,12 @@ my_tuple = (0, 1, 2, 3, 4, 5, 6)
 foo = tuple(filter(lambda x: x>1, my_tuple)) # Insertar línea de código aquí.
 print(foo)
 
+def I():
+    s = 'abcdef'
+    for c in s[::2]:
+        yield c
+
+
+for x in I():
+    print(x, end='')
+
